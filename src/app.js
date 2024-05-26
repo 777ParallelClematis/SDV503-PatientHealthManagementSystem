5b760888f1184e19282db238280a95d26846d5da
Erin Colane <erin-colane@live.nmit.ac.nz>
2024-05-26 19:57:12 +1200

    structure for login/menu display etc.

--- src/app.js
+++ src/app.js
@@ -5,5 +5,22 @@ const readlineSync = require('readline-sync')
 
 // display patient menu
 
-//display medical professional menu 
+// display medical professional menu 
 
+// patient option one:
+// view profile
+
+// patient option two:
+// update details
+
+//patient option three:
+// contact assigned medprofessional
+
+// medpro option one:
+// see patient (enter ID or full name)
+
+//medpro option two:
+//update patient details
+
+//medpro option three:
+//register new patient
