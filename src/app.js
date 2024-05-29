9461ce8a78475bb978f0171993ceb10affd3ec0b
Erin Colane <erin-colane@live.nmit.ac.nz>
2024-05-29 14:58:10 +1200

    3 menus begun, readline-sync chosen

--- src/app.js
+++ src/app.js
@@ -2,25 +2,24 @@ const readlineSync = require('readline-sync')
 
 
 //display a log in 
+// (readline) menu to choose 1 (medical professional) or 2 (patient)
+const logInChoice = ["Medical Professional, Hippocrates", "Patient, Carly Closs"]
 
-// display patient menu
 
-// display medical professional menu 
 
-// patient option one:
-// view profile
+// display patient menu 
+const patientOptions = ["See profile", "Contact Medical Professional", "Exit"]
+// readline to either:
+// 1 See profile
+// 2 Contact medical professional (readline sync and nodemailer)
+// 3 Exit
 
-// patient option two:
-// update details
 
-//patient option three:
-// contact assigned medprofessional
 
-// medpro option one:
-// see patient (enter ID or full name)
+// display medpro menu 
+const medProOptions = ["View Patient/s", "Add patient", "Exit"]
+// readline to either:
+// 1 View Patients ()
+// 2 Add patient ()
+// 3 Exit
 
-//medpro option two:
-//update patient details
-
-//medpro option three:
-//register new patient
