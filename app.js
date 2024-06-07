087f34670263e1c3fa568a5400c1e2b3c76d6c5a
Erin Colane <erin-colane@live.nmit.ac.nz>
2024-06-07 21:26:24 +1200

    iniitial menu structure

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
-const readlineSync = require('readline-sync')
+import inquirer from 'inquirer'
 
 
-//display a log in 
+// function to show initial menu
 
-// display patient menu
 
-//display medical professional menu 
+// Function to show initial menu
+async function initialMenu() {
+    const questions = [{
+        type: "list",
+        name: "viewOptions",
+        message: "Welcome to RecordPortal 1.0",
+        choices: ["I am a Medical Professional", "I am a Patient", "Other, Contact Developer"]
+    }]
+    
+    const answers = await inquirer.prompt(questions);
+    if (answers.viewOptions === "I am a Medical Professional") {
+        medicalProfessionalMenu();
+    } else if (answers.viewOptions === "I am a Patient") {
+        patientMenu()
+    } else {
+        console.log("Contact Developer option selected, enter the message you wish to send")
+        readline
+    }
+}
 
+initialMenu();
+
+function medicalProfessionalMenu() {
+    console.log("Medical Professional Menu loading ...")
+}
+
+function patientMenu() {
+    console.log("Patient menu loading ...")
+}
+
+function 
+
+
+// patient menu options
+
+// medpro menu options
